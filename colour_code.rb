0d2a03ec26e4769af14abf3586691d56de626ccf
ilstar <xingqinglzq@gmail.com>
2011-03-30 21:02:16 +0800

    add multi-lang support

--- colour_code.rb
+++ colour_code.rb
@@ -2,12 +2,14 @@ require 'rubygems'
 require 'sinatra'
 require 'coderay'
 
+CODE_LANGS = %w{Ruby C CSS Delphi HTML JSON Java JavaScript PHP Python RHTML SQL YAML}
+
 get '/' do
   erb :index
 end
 
 post '/' do
-  @code = CodeRay.scan(params[:code], 'ruby').div(:line_numbers => :table)
+  @code = CodeRay.scan(params[:code], params[:lang]).div(:line_numbers => :table)
   
   erb :code
 end
